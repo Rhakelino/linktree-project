fd4d0b50182835a0ea7109e96f3532675450751f
rakel lino <instanclay@gmail.com>
2024-11-29 00:21:45 +0700

    back to first design

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,25 @@ function App() {
         <div className="flex justify-center">
           <img src="images/rhakel.jpg" className="w-28 h-28 object-cover rounded-full" />
         </div>
-        <div className="flex flex-col text-center mt-3">
+        <div className="flex flex-col text-center gap-2 mt-3">
           <h1 className="font-bold text-xl">Rhakelino</h1>
-          <p className="font-medium text-sm">Link Projects</p>
-          <a href="https://calculatoor-apps.netlify.app/" target="_blank">
-            <div className="flex mt-5 bg-white text-black font- w-96 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
-              Calculator App
-            </div>
-          </a>
-          <a href="https://juju-otaku.netlify.app/" target="_blank">
-            <div className="flex mt-5 bg-white text-black font- w-96 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
-              Web Anime List
-            </div>
-          </a>
-          <a href="https://juju-ai.netlify.app/" target="_blank">
-            <div className="flex mt-5 bg-white text-black font- w-96 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
-              Juju AI
-            </div>
-          </a>
+          <p className="font-medium text-sm text-slate-400">Link Projects</p>
         </div>
+        <a href="https://calculatoor-apps.netlify.app/" target="_blank">
+          <div className="flex mt-5 bg-white text-black font- w-80 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
+            Calculator App
+          </div>
+        </a>
+        <a href="https://juju-otaku.netlify.app/" target="_blank">
+          <div className="flex mt-5 bg-white text-black font- w-80 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
+            Web Anime List
+          </div>
+        </a>
+        <a href="https://juju-ai.netlify.app/" target="_blank">
+          <div className="flex mt-5 bg-white text-black font- w-80 h-12 justify-center items-center rounded-full shadow-md font-bold hover:bg-slate-900 hover:text-white">
+            JUJU AI
+          </div>
+        </a>
       </div>
     </div>
   )
